0ac1b93030787ce578d1253158ea59787992c5df
André Marques <andre.m.d.abreu@gmail.com>
2023-08-03 11:56:06 -0300

    Desafio quase finalizado

--- exercicios/exercicio-05.php
+++ exercicios/exercicio-05.php
@@ -15,13 +15,15 @@
 </head>
 <body>
     <?php
-        $nota1 = 8;
+        $nota1 = 6;
         $nota2 = 7;
+        
+
         function calculaMedia(float $nota1, $nota2):float {
             $media = ($nota1 + $nota2) / 2;
             return $media;
         }
-
+        
         function situacao(float $media):string {
             if ($media >= 7) {
                 $resultado = "<span class='aprovado'>aprovado!</span>";
@@ -30,10 +32,38 @@
             };
             return $resultado;
         };
+
+        $mediaFinal = calculaMedia($nota1, $nota2);
+
+
     ?>
 
     <h1>Médias dos patetas</h1>
-    <p>O aluno fulanito teve média <?=calculaMedia($nota1, $nota2)?>, então ele foi <?=situacao(calculaMedia($nota1, $nota2))?></p>
+    <p>O aluno fulanito teve média <?=calculaMedia($nota1, $nota2)?>, então ele foi <?=situacao($mediaFinal)?></p>
 
+    <hr>
+
+    <h2>Desafio dos Desafios mais Desafiadores</h2>
+    <ol>
+        <?php
+            $alunos = ["Motocó", "Tiulielson", "Kinder Bueno", "Pelipe", "Leandro do Arrocha"];
+            $notas = [
+                [8, 9],
+                [7, 10],
+                [1, 6],
+                [6, 7],
+                [7, 7]
+            ];
+
+            for ($i = 0; $i < 5; $i++) { 
+                for ($j = 0; $j < 2; $j++) {
+                    $notaLoop0 = $notas[$j];
+                }
+            ?>
+                <li><?=$alunos[$i]?>, <?=calculaMedia(8, 7)?>, <?=situacao(calculaMedia(8, 7))?></li>
+            <?php
+            }
+        ?>
+    </ol>
 </body>
 </html>
