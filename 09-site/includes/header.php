77b79ff90ff2b2c84f1b6e472e4445c2b2af44c5
André Marques <andre.m.d.abreu@gmail.com>
2023-08-08 09:23:15 -0300

    Atualização de variável

--- 09-site/includes/header.php
+++ 09-site/includes/header.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$pagina?> - Site PHP</title>
+    <title><?=$titulo?> - Site PHP</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
